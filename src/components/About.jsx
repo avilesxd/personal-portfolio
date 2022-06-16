e3e3fe0994e8d82563598f467b1893e1b2d66f39
avilesxd <nacho72001@gmail.com>
2022-06-16 14:54:12 -0400

    modify about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,8 @@ function About() {
 			<div className="w-full md:w-6/12">
 				<SectionTitle>About Me</SectionTitle>
 				<p className="text-md text-gray-600 dark:text-gray-300">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-					veniam dolor consectetur pariatur explicabo, iure nulla. Dolor
-					debitis, natus cum ad, fugiat excepturi minima culpa atque modi
-					accusantium vel voluptatem?
+					My name is Ignacio Avilés, I am 20 years old. I am a computer
+					engineering student at the University of Santo Tomas Arica, Chile.
 				</p>
 				<a
 					href="mailto:nacho72001@gmail.com"
