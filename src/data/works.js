ad17626b09b2512111bc13662b40ed414473e4d5
avilesxd <nacho72001@gmail.com>
2022-06-16 12:46:58 -0400

    initial commit

---
+++ src/data/works.js
@@ -0,0 +1,9 @@
+export default [
+	{
+		title: 'Personal Portfolio',
+		imgUrl:
+			'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+		tech: ['Vite JS', 'React JS', 'Tailwind CSS'],
+		workUrl: '',
+	},
+];
