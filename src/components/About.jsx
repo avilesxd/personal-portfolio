75f04e743834aedb63df59822df198c2e192dfb2
avilesxd <nacho72001@gmail.com>
2022-06-16 23:15:49 -0400

    modify about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,12 +11,6 @@ function About() {
 					My name is Ignacio Avilés, I am 20 years old. I am a computer
 					engineering student at the University of Santo Tomas Arica, Chile.
 				</p>
-				<a
-					href="mailto:nacho72001@gmail.com"
-					className="block mt-3 text-md md:text-lg text-gray-700 dark:text-gray-300 underline decoration-1 hover:text-indigo-500 dark:hover:text-indigo-500"
-				>
-					nacho72001@gmail.com
-				</a>
 			</div>
 
 			<img
