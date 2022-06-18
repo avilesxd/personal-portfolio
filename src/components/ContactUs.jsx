6a4329233d3cc189c4aed93b7550b65c6cd32e19
avilesxd <nacho72001@gmail.com>
2022-06-18 15:29:04 -0400

    modify contact us

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -19,30 +19,30 @@ export const ContactUs = () => {
     }
 
     return (
-        <div className='mb-3 '>
-            <form onSubmit={ sendEmail }>
+        <div className='w-full content-center'>
+            <form autoComplete='off' onSubmit={ sendEmail }>
                 <div className='text-center text-md md:text-xl mb-3'>
                     <label className='text-md md:text-xl max-w-md mb-3 font-semibold text-gray-600 dark:text-gray-300'>Name</label>
                     <div>
-                        <input type="text" name='user_name' className='bg-slate-300 dark:bg-slate-300 rounded-lg overflow-hidden' />
+                        <input type="text" name='user_name' required className='bg-slate-300 dark:bg-slate-300 rounded-lg overflow-hidden' />
                     </div>
                 </div>
 
                 <div className='text-center text-md md:text-xl mb-3'>
                     <label className='text-md md:text-xl max-w-md mb-3 font-semibold text-gray-600 dark:text-gray-300'>Email</label>
                     <div>
-                        <input type="email" name='user_email' className='bg-slate-300 dark:bg-slate-300 rounded-lg overflow-hidden' />
+                        <input type="email" name='user_email' required className='bg-slate-300 dark:bg-slate-300 rounded-lg overflow-hidden' />
                     </div>
                 </div>
 
                 <div className='text-center text-md md:text-xl mb-3'>
                     <label className='text-md md:text-xl max-w-md mb-3 font-semibold text-gray-600 dark:text-gray-300'>Message</label>
                     <div>
-                        <textarea name="user_message" id="" cols="30" rows="10" className='bg-slate-300 dark:bg-slate-300 rounded-lg overflow-hidden'></textarea>
+                        <textarea name="user_message" id="" cols="30" rows="10" required className='bg-slate-300 dark:bg-slate-300 rounded-lg overflow-hidden'></textarea>
                     </div>
                 </div>
                 <div className='text-center text-md md:text-xl mb-3'>
-                    <button className="inline-block px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-md">Send</button>
+                    <button type='submit' value='Send' className="inline-block px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-md">Send</button>
                 </div>
             </form>
         </div>
