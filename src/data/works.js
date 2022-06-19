90af12c5cc471d19937c4bd86e62154d0ad62b69
avilesxd <nacho72001@gmail.com>
2022-06-19 03:16:00 -0400

    modify works

--- src/data/works.js
+++ src/data/works.js
@@ -3,7 +3,7 @@ export default [
 		title: 'Personal Portfolio',
 		imgUrl:
 			'https://raw.githubusercontent.com/avilesxd/persoanl-portfolio/master/src/images/Work1.jpg',
-		tech: ['Vite JS', 'React JS', 'Tailwind CSS'],
+		tech: ['Vite JS', 'React JS', 'Email JS', 'Tailwind CSS'],
 		workUrl: 'https://github.com/avilesxd/personal-portfolio',
 	},
 ];
