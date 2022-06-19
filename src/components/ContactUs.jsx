e3334d35b0792032202b1dc0016d07688ac33a3a
avilesxd <nacho72001@gmail.com>
2022-06-19 01:20:25 -0400

    modify contact us

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -25,21 +25,21 @@ export const ContactUs = () => {
                 <div className='text-center text-md md:text-xl mb-3'>
                     <label className='text-md md:text-xl max-w-md mb-3 font-semibold text-gray-600 dark:text-gray-300'>Name</label>
                     <div>
-                        <input type="text" name='user_name' required placeholder='Name' className='bg-slate-300 dark:bg-slate-300 rounded-lg overflow-hidden' />
+                        <input type="text" name='user_name' required placeholder='Name' className='bg-slate-300 dark:bg-slate-300 rounded-lg px-3 overflow-hidden' />
                     </div>
                 </div>
 
                 <div className='text-center text-md md:text-xl mb-3'>
                     <label className='text-md md:text-xl max-w-md mb-3 font-semibold text-gray-600 dark:text-gray-300'>Email</label>
                     <div>
-                        <input type="email" name='user_email' required placeholder='Example@gmail.com' className='bg-slate-300 dark:bg-slate-300 rounded-lg overflow-hidden' />
+                        <input type="email" name='user_email' required placeholder='Example@gmail.com' className='bg-slate-300 dark:bg-slate-300 rounded-lg px-3 overflow-hidden' />
                     </div>
                 </div>
 
                 <div className='text-center text-md md:text-xl mb-3'>
                     <label className='text-md md:text-xl max-w-md mb-3 font-semibold text-gray-600 dark:text-gray-300'>Message</label>
                     <div>
-                        <textarea name="user_message" id="" cols="30" rows="10" required placeholder='Your message' className='bg-slate-300 dark:bg-slate-300 rounded-lg overflow-hidden'></textarea>
+                        <textarea name="user_message" id="" cols="30" rows="10" required placeholder='Your message' className='bg-slate-300 dark:bg-slate-300 rounded-lg px-3 overflow-hidden'></textarea>
                     </div>
                 </div>
                 <div className='text-center text-md md:text-xl mb-3'>
